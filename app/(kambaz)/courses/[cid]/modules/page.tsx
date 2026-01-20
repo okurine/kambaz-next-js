14d5ef85e125846d79be83f96c44799c399abf9b
xuell06 <xu.ell@northeastern.edu>
2026-01-20 14:34:41 -0500

    reformat module code

--- app/(kambaz)/courses/[cid]/modules/page.tsx
+++ app/(kambaz)/courses/[cid]/modules/page.tsx
@@ -12,52 +12,72 @@ export default function Modules() {
       </select>
       <button id="wd-add-module-add-module">+ Module</button>
       <ul id="wd-modules">
+        {/* Course content */}
         <li className="wd-module">
-          <div className="wd-title">Week 1, Lecture 1 - Course Introductions, 
-            Syllabus, Agenda</div>
+          <div className="wd-title">
+            Week 1, Lecture 1 - Course Introductions, Syllabus, Agenda
+          </div>
           <ul className="wd-lessons">
             <li className="wd-lesson">
               <span className="wd-title">LEARNING OBJECTIVES</span>
               <ul className="wd-content">
                 <li className="wd-content-item">Introduction to the course</li>
-                <li className="wd-content-item">Learn what is Web Development</li>
+                <li className="wd-content-item">
+                  Learn what is Web Development
+                </li>
               </ul>
               <span className="wd-title">READING</span>
               <ul className="wd-content">
-                <li className="wd-content-item">Full Stack Developer - Chapter 1 
-                  - Introduction</li>
-                <li className="wd-content-item">Full Stack Developer - Chapter 2 
-                  - Creating User</li>
+                <li className="wd-content-item">
+                  Full Stack Developer - Chapter 1 - Introduction
+                </li>
+                <li className="wd-content-item">
+                  Full Stack Developer - Chapter 2 - Creating User
+                </li>
               </ul>
               <span className="wd-title">SLIDES</span>
               <ul className="wd-content">
-                <li className="wd-content-item">Introduction to Web Development</li>
-                <li className="wd-content-item">Creating an HTTP server with Node.js</li>
-                <li className="wd-content-item">Creating a React Application</li>
+                <li className="wd-content-item">
+                  Introduction to Web Development
+                </li>
+                <li className="wd-content-item">
+                  Creating an HTTP server with Node.js
+                </li>
+                <li className="wd-content-item">
+                  Creating a React Application
+                </li>
               </ul>
             </li>
           </ul>
         </li>
         <li className="wd-module">
-          <div className="wd-title">Week 2, Lecture 2 - Formatting User Interface with 
-            HTML</div>
+          <div className="wd-title">
+            Week 2, Lecture 2 - Formatting User Interface with HTML
+          </div>
           <ul className="wd-lessons">
             <li className="wd-lesson">
               <span className="wd-title">LEARNING OBJECTIVES</span>
             </li>
             <ul className="wd-content">
-                <li className="wd-content-item">Learn how to create user interfaces with
-                  HTML
-                </li>
-                <li className="wd-content-item">Deploy the assignment to Vercel</li>
-              </ul>
-              <span className="wd-title">SLIDES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Introduction to HTML and the DOM</li>
-                <li className="wd-content-item">Formatting Web content with Headings</li>
-                <li className="wd-content-item">Formatting content with Lists and Tables
-                </li>
-              </ul>
+              <li className="wd-content-item">
+                Learn how to create user interfaces with HTML
+              </li>
+              <li className="wd-content-item">
+                Deploy the assignment to Vercel
+              </li>
+            </ul>
+            <span className="wd-title">SLIDES</span>
+            <ul className="wd-content">
+              <li className="wd-content-item">
+                Introduction to HTML and the DOM
+              </li>
+              <li className="wd-content-item">
+                Formatting Web content with Headings
+              </li>
+              <li className="wd-content-item">
+                Formatting content with Lists and Tables
+              </li>
+            </ul>
           </ul>
         </li>
         <li className="wd-module">
@@ -65,4 +85,5 @@ export default function Modules() {
         </li>
       </ul>
     </div>
-);}
+  );
+}
