36dac95a806222075966ad708dff56f9db421d9f
xuell06 <xu.ell@northeastern.edu>
2026-01-20 14:10:47 -0500

    add buttons to home page

--- app/(kambaz)/courses/[cid]/home/status.tsx
+++ app/(kambaz)/courses/[cid]/home/status.tsx
@@ -2,7 +2,14 @@ export default function CourseStatus() {
   return (
     <div id="wd-course-status">
       <h2>Course Status</h2>
-      <button>Unpublish</button> <button>Publish</button>
+      <button>Unpublish</button> <button>Publish</button><br></br>
       {/* Complete on your own */}
+
+      <button>Import Existing Content</button><br></br>
+      <button>Import from Content</button><br></br>
+      <button>Choose Home Page</button><br></br>
+      <button>View Course Stream</button><br></br>
+      <button>New Announcement</button><br></br>
+      <button>New Analytics</button><br></br>
       <button>View Course Notifications</button>
     </div> );}
