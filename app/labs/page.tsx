fd05b0731f696d8f486e0e0adbfcb83c97ec74ae
xuell06 <xu.ell@northeastern.edu>
2026-01-21 15:40:11 -0500

    add name and section to labs page

--- app/labs/page.tsx
+++ app/labs/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 export default function labs() {
  return (
    <div id="wd-labs">
-     <h1>Labs</h1>
+     <h1>Ella Xu's Labs (CS4550.33211)</h1>
      <ul>
        <li>
          <Link href="/labs/lab1" id="wd-lab1-link">
