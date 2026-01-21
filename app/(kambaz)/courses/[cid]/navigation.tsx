b12d87744a7a3abe2b63314b4fca02d05ffcfdc1
xuell06 <xu.ell@northeastern.edu>
2026-01-21 15:40:40 -0500

    modify piazza and zoom links

--- app/(kambaz)/courses/[cid]/navigation.tsx
+++ app/(kambaz)/courses/[cid]/navigation.tsx
@@ -5,13 +5,17 @@ export default function CourseNavigation() {
       <Link href="/courses/1234/home" id="wd-course-home-link">Home</Link><br/>
       <Link href="/courses/1234/modules" id="wd-course-modules-link">Modules
         </Link><br/>
-      <Link href="/courses/1234/piazza" id="wd-course-piazza-link">Piazza</Link><br/>
-      <Link href="/courses/1234/zoom" id="wd-course-zoom-link">Zoom</Link><br/>
+      {/* <Link href="/courses/1234/piazza" id="wd-course-piazza-link">Piazza</Link><br/> */}
+      <a href="https://piazza.com/" id="wd-course-piazza-link" target="_blank">Piazza</a><br/>
+      {/* <Link href="/courses/1234/zoom" id="wd-course-zoom-link">Zoom</Link><br/> */}
+      <a href="https://zoom.us/signin#/login" id="wd-course-zoom-link" target="_blank">Zoom</a><br/>
       <Link href="/courses/1234/assignments" id="wd-course-assignments-link">
           Assignments</Link><br/>
       <Link href="/courses/1234/quizzes" id="wd-course-quizzes-link">Quizzes
         </Link><br/>
       <Link href="/courses/1234/grades" id="wd-course-grades-link">Grades</Link><br/>
-      <Link href="/courses/1234/people/table" id="wd-course-people-link">People</Link><br/>
+      {/* <Link href="/courses/1234/people/table" id="wd-course-people-link">People</Link><br/> */}
+      <Link href="/courses/1234/people" id="wd-course-people-link">People</Link><br/>
+
     </div>
   );}
