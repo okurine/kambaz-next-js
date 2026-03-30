6bffa0e32c53337cd58e6b553da4491cfaf105f3
xuell06 <xu.ell@northeastern.edu>
2026-03-29 22:13:15 -0400

    fix deployment issues

--- app/(kambaz)/courses/[cid]/modules/page.tsx
+++ app/(kambaz)/courses/[cid]/modules/page.tsx
@@ -27,9 +27,12 @@ export default function Modules() {
 
   const onCreateModuleForCourse = async () => {
     if (!cid) return;
-    const newModule = { name: moduleName, course: cid };
-    const module = await client.createModuleForCourse(cid, newModule);
-    dispatch(setModules([...modules, module]));
+    const newModule = { name: moduleName, course: cid as string };
+    const createdModule = await client.createModuleForCourse(
+      cid as string,
+      newModule,
+    );
+    dispatch(setModules([...modules, createdModule]));
   };
 
   const onRemoveModule = async (moduleId: string) => {
