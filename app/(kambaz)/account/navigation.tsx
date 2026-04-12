3d307872ed4d418258c89c4f61fae2480563f2bf
xuell06 <xu.ell@northeastern.edu>
2026-04-12 05:09:06 -0400

    branch A6

--- app/(kambaz)/account/navigation.tsx
+++ app/(kambaz)/account/navigation.tsx
@@ -14,7 +14,11 @@ export default function AccountNavigation() {
      {links.map((link) => (
        <NavItem key={link}>
          <NavLink as={Link} href={link} active={pathname.endsWith(link)}>
-           {link} </NavLink> </NavItem>
+           {link} </NavLink> 
+             {currentUser && currentUser.role === "ADMIN" && (
+              <NavLink as={Link} href={`/account/users`}  active={pathname.endsWith('Users')}> Users </NavLink> )}
+        </NavItem>
+
      ))}
    </Nav>
 );}
--- app/(kambaz)/courses/[cid]/people/table/page.tsx
+++ app/(kambaz)/courses/[cid]/people/table/page.tsx
@@ -1,15 +1,30 @@
 "use client";
 import { Table } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
-import * as db from "../../../../database";
-import { useParams } from "next/navigation";
+import PeopleDetails from "../Details";
+import { useState } from "react";
 
-export default function PeopleTable() {
-  const { cid } = useParams();
-  const { users, enrollments } = db;
-
+export default function PeopleTable({
+  users = [],
+  fetchUsers,
+}: {
+  users?: any[];
+  fetchUsers: () => void;
+}) {
+  const [showDetails, setShowDetails] = useState(false);
+  const [showUserId, setShowUserId] = useState<string | null>(null);
   return (
     <div id="wd-people-table">
+      {showDetails && (
+        <PeopleDetails
+          uid={showUserId}
+          onClose={() => {
+            setShowDetails(false);
+            fetchUsers();
+          }}
+        />
+      )}
+
       <Table striped>
         <thead>
           <tr>
@@ -22,27 +37,28 @@ export default function PeopleTable() {
           </tr>
         </thead>
         <tbody>
-          {users
-            .filter((usr) =>
-              enrollments.some(
-                (enrollment) =>
-                  enrollment.user === usr._id && enrollment.course === cid,
-              ),
-            )
-            .map((user: any) => (
-              <tr key={user._id}>
-                <td className="wd-full-name text-nowrap">
+          {users.map((user: any) => (
+            <tr key={user._id}>
+              <td className="wd-full-name text-nowrap">
+                <span
+                  className="text-decoration-none"
+                  onClick={() => {
+                    setShowDetails(true);
+                    setShowUserId(user._id);
+                  }}
+                >
                   <FaUserCircle className="me-2 fs-1 text-secondary" />
-                  <span className="wd-first-name">{user.firstName}</span>
+                  <span className="wd-first-name">{user.firstName}</span>{" "}
                   <span className="wd-last-name">{user.lastName}</span>
-                </td>
-                <td className="wd-login-id">{user.loginId}</td>
-                <td className="wd-section">{user.section}</td>
-                <td className="wd-role">{user.role}</td>
-                <td className="wd-last-activity">{user.lastActivity}</td>
-                <td className="wd-total-activity">{user.totalActivity}</td>
-              </tr>
-            ))}
+                </span>
+              </td>
+              <td className="wd-login-id">{user.loginId}</td>
+              <td className="wd-section">{user.section}</td>
+              <td className="wd-role">{user.role}</td>
+              <td className="wd-last-activity">{user.lastActivity}</td>
+              <td className="wd-total-activity">{user.totalActivity}</td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </div>
